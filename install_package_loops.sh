6556ba21f3de04c1b628077e5a21e7b0590f80c9
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-09-23 23:51:10 +0530

    Package install practice

--- install_package_loops.sh
+++ install_package_loops.sh
@@ -4,16 +4,21 @@ USER_ID=$(id -u)
 LOGS_FILE="/var/log/shell-script/$LOGS_FILE_NAME.log"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 if [ $USER_ID -ne 0 ]; then 
-    echo "$TIMESTAMP [INFO] : USer do not have permissions to run this script: Please run with sudo access" | tee -a $LOGS_FILE
+    echo -e "$TIMESTAMP $Y [INFO] : $N USer do not have permissions to run this script: Please run with sudo access" | tee -a $LOGS_FILE
     exit 1
 fi
 
 VALIDATE() {
     if [ $2 -ne 0 ]; then
-        echo "$TIMESTAMP [ERROR] :  Installing $1 is failed" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP $R [ERROR] :  Installing $1 is failed" | tee -a $LOGS_FILE
     else
-        echo "$TIMESTAMP [INFO] : Installing the package $1 is success" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP $G [INFO] : Installing the package $1 is success" | tee -a $LOGS_FILE
     fi
 }
 
@@ -21,10 +26,10 @@ for package in $@
     do
         dnf list installed $package
         if [ $? -eq 0 ]; then
-            echo "$TIMESTAMP [INFO]: Required $package is already installed skipping" | tee -a $LOGS_FILE
+            echo -e "$TIMESTAMP $Y [INFO]: $N Required $package is already installed skipping" | tee -a $LOGS_FILE
             #exit 1
         else 
-            echo "$TIMESTAMP [INFO] Installing $package....." | tee -a $LOGS_FILE
+            echo -e "$TIMESTAMP $Y [INFO] Installing $package....." | tee -a $LOGS_FILE
             dnf install $package -y &>>$LOGS_FILE
              VALIDATE $package $?
         fi
