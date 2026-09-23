21c83d7867082203a695d4cf0ab74ecfa7294ab3
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-09-23 23:18:16 +0530

    Package install practice

--- package_install_practice.sh
+++ package_install_practice.sh
@@ -4,25 +4,25 @@ USER_ID=$(id -u)
 LOGS_FILE="/var/log/shell-script/$LOGS_FILE_NAME.log"
 
 if [ $USER_ID -ne 0 ]; then 
-    echo "USer do not have permissions to run this script: Please run with sudo access"
+    echo "USer do not have permissions to run this script: Please run with sudo access" | tee -a $LOGS_FILE
     exit 1
 fi
 
 VALIDATE() {
     if [ $2 -ne 0 ]; then
-        echo "Installing $1 is failed"
+        echo "Installing $1 is failed" | tee -a $LOGS_FILE
     else
-        echo "Installing the package $1 is success"
+        echo "Installing the package $1 is success" | tee -a $LOGS_FILE
     fi
 }
 
 dnf list installed mysql &>>$LOGS_FILE
 
 if [ $? -eq 0 ]; then
-    echo "Required package mysql is already installed skipping"
+    echo "Required package mysql is already installed skipping" | tee -a $LOGS_FILE
     exit 1
 else 
-    echo "Installing My SQL....."
+    echo "Installing My SQL....." | tee -a $LOGS_FILE
     dnf install mysql -y &>>$LOGS_FILE
 
 fi
